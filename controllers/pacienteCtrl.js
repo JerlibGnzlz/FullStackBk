51a36b37963d2cc739a5d2962e62c234c8a3226b
jerlibgnzlz <jerlibgv@gmail.com>
2023-04-13 16:24:07 -0300

    Subiendo el update y delete del paciente

--- controllers/pacienteCtrl.js
+++ controllers/pacienteCtrl.js
@@ -36,25 +36,75 @@ export const ObtenerPaciente = async (req, res) => {
     const { id } = req.params;
     const paciente = await PacienteModel.findById(id);
 
+    if (!paciente) {
+        res.status(400).json({ msg: "No encontrado" });
+    }
+
     //Cuando se comparan dos objetId se les coloca toString()
     if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
         return res.json({ msg: "Accion no valida" });
     }
-    if (paciente) {
-        res.json(paciente);
 
-    }
+    res.json(paciente);
+
 
 };
 
 
 export const actualizarPaciente = async (req, res) => {
 
+    const { id } = req.params;
+    // const paciente = new PacienteModel(req.body);
 
+    const paciente = await PacienteModel.findById(id);
+
+
+    if (!paciente) {
+        res.status(400).json({ msg: "No encontrado" });
+    }
+
+    //Cuando se comparan dos objetId se les coloca toString()
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({ msg: "Accion no valida" });
+    }
+
+    paciente.nombre = req.body.nombre || paciente.nombre;
+    paciente.propietario = req.body.propietario || paciente.propietario;
+    paciente.email = req.body.email || paciente.email;
+    paciente.fecha = req.body.fecha || paciente.fecha;
+    paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+    try {
+        const pacienteActualizado = await paciente.save();
+        res.json(pacienteActualizado);
+    } catch (error) {
+        console.log(error);
+    }
+    // res.json(pacienteActualizado);
 };
+
 
 export const eliminarPaciente = async (req, res) => {
 
+    const { id } = req.params;
+    // const paciente = new PacienteModel(req.body);
 
+    const paciente = await PacienteModel.findById(id);
+
+
+    if (!paciente) {
+        res.status(400).json({ msg: "No encontrado" });
+    }
+
+    //Cuando se comparan dos objetId se les coloca toString()
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({ msg: "Accion no valida" });
+    }
+    try {
+        await paciente.deleteOne();
+        res.json({ msg: "Paciente eliminado" });
+    } catch (error) {
+        console.log(error);
+    }
 
 };
